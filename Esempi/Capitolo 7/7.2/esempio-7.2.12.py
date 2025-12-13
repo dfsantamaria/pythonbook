377e571c06731d6439dbf3abbf046054a5c14b5b
Daniele Francesco Santamaria <dfsantamaria@users.noreply.github.com>
2025-12-13 12:18:59 +0100

    esempi aggiornati

---
+++ Esempi/Capitolo 7/7.2/esempio-7.2.12.py
@@ -0,0 +1,7 @@
+#Accoppiare elementi di due liste
+lista1 = [1, 2, 3]
+lista2 = ['a', 'b', 's']
+coppie = []
+for i in range(len(lista1)):
+    coppie.append((lista1[i], lista2[i]))
+
---
+++ Esempi/Capitolo 7/7.2/esempio-7.2.13.py
@@ -0,0 +1,4 @@
+#La funzione zip
+lista1 = [1, 2, 3]
+lista2 = ['a', 'b', 's']
+coppie = list(zip(lista1,lista2))
